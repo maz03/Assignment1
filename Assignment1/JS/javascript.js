a192dd77ea87dcf0e0eed62b9621debf45503387
maz03 <83689332+maz03@users.noreply.github.com>
2022-09-07 10:21:09 +1200

    Delete Assignment1 directory

--- Assignment1/JS/javascript.js
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------
-//CURTAIN MENU
-function openNav() {
-	document.getElementById("curtain-nav").style.width = "50%";			
-}
-
-function closeNav() {
-	document.getElementById("curtain-nav").style.width = "0%";			
-}		
