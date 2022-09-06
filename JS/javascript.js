cd49d7aa0981c93f065569923750f778db0d71de
maz03 <83689332+maz03@users.noreply.github.com>
2022-09-07 10:21:58 +1200

    Add files via upload

---
+++ JS/javascript.js
@@ -0,0 +1,9 @@
+//-----------------------------------------------------
+//CURTAIN MENU
+function openNav() {
+	document.getElementById("curtain-nav").style.width = "50%";			
+}
+
+function closeNav() {
+	document.getElementById("curtain-nav").style.width = "0%";			
+}		
